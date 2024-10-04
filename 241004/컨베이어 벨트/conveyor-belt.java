fbc3538e1192d2daa41724b6aa90fc9f5425488c
Binfoo <julie0005@ajou.ac.kr>
2024-10-04 21:55:44 +0900

    Title: 컨베이어 벨트, Time: 184ms, Memory: 11MB, Status: Passed - CodeTree

--- 241004/컨베이어 벨트/conveyor-belt.java
+++ 241004/컨베이어 벨트/conveyor-belt.java
@@ -2,44 +2,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Queue<Integer> up = new LinkedList<>();
-        Queue<Integer> down = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int t = sc.nextInt();
+        int[] belt = new int[n*2];
 
-        int[] ups = new int[n];
-        int[] downs = new int[n];
-        for (int i=0; i<n; i++) {
-            ups[i] = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            belt[i + n] = sc.nextInt();
         }
-        for (int i=0; i<n; i++) {
-            downs[i] = sc.nextInt();
-        }
-
-        for (int i=n-1; i>=0; i--) {
-            up.add(ups[i]);
-            down.add(downs[i]);
+        for(int i=0; i<n; i++) {
+            belt[i] = sc.nextInt();
         }
 
-        int currentTime = 0;
-        while (currentTime++ < t) {
-            int temp = up.remove();
-            down.add(temp);
-            temp = down.remove();
-            up.add(temp);
+        int cur = 0;
+        while(cur++ < t) {
+            int temp = belt[2*n - 1];
+            for (int i=2*n-2; i>=0; i--) {
+                belt[i+1] = belt[i];
+            }
+            belt[0] = temp;
         }
 
-        for (int i=0; i<n; i++) {
-            ups[n - i - 1] = up.remove();
-            downs[n - i - 1] = down.remove();
-        }
-        for (int i=0; i<n; i++) {
-            System.out.print(ups[i] + " ");
+        for(int i=0; i<n; i++) {
+            System.out.print(belt[i + n] + " ");
         }
         System.out.println();
-        for (int i=0; i<n; i++) {
-            System.out.print(downs[i] + " ");
+        for(int i=0; i<n; i++) {
+            System.out.print(belt[i] + " ");
         }
+
     }
 }
