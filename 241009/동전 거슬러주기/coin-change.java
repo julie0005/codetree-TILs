1a9644ccf1ea2af11897151946805f405c428215
Binfoo <julie0005@ajou.ac.kr>
2024-10-09 01:39:40 +0900

    Title: 동전 거슬러주기, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/동전 거슬러주기/coin-change.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] dp = new int[m+1];
+        int[] coins = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i=0; i<n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        dp[0] = 0;
+        for(int i=1; i<=m; i++) {
+            for(int j=0; j<n; j++) {
+                if (i >= coins[j]) {
+                    if (dp[i - coins[j]] == Integer.MAX_VALUE) continue;
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+        System.out.println(dp[m]);
+    }
+}
