f46c5096732a0291d2e0c62345500733d4ec924d
Binfoo <julie0005@ajou.ac.kr>
2024-10-09 23:47:21 +0900

    Title: n개 중에 m개 뽑기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/n개 중에 m개 뽑기/n-choose-m.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    static List<Integer> num = new ArrayList<>();
+    static List<String> seqs = new ArrayList<>();
+    static int K,N;
+    static void addToSeqs() {
+        String str = "";
+        for (int i=0; i<num.size(); i++) {
+            str += num.get(i);
+            if (i < num.size()-1) str+=" ";
+        }
+        seqs.add(str);
+    }
+    static void choose(int idx, int lastNum) {
+        if (idx == N) {
+            addToSeqs();
+            return;
+        }
+
+        for(int i=1; i<=K; i++) {
+            if (i <= lastNum) continue;
+            num.add(i);
+            choose(idx+1, i);
+            num.remove(num.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        K = sc.nextInt();
+        N = sc.nextInt();
+        choose(0, 0);
+        Collections.sort(seqs);
+        for(String seq : seqs) {
+            System.out.println(seq);
+        }
+    }
+}
